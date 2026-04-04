845f8086b1eb8897410a92b445cbf0d8f98f97ef
Gavin May <gav.s.may@gmail.com>
2026-04-04 18:42:49 +0100

    changes

--- backend/db.example.php
+++
@@ -1,7 +0,0 @@
-<?php
-return [
-  "servername" => "localhost",
-  "username" => "your_db_user",
-  "password" => "your_db_password",
-  "dbname" => "loop_db",
-];
--- backend/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-session_unset();
-session_destroy();
-
-header("Location: ../templates/login_page.php");
-exit();
-?>
